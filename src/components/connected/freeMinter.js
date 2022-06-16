8ba3acd4655fb10440108fc4c2d382bb9a7b8323
Elbert Nathanael <elbertwithane@gmail.com>
2022-06-16 22:14:26 +0800

    fixed agent

--- src/components/connected/freeMinter.js
+++ src/components/connected/freeMinter.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { web3Module } from "../authContext";
 
 export default function FreeMinter(prop){
   const [mintAble, setMintAble] = useState(true)
@@ -10,7 +11,7 @@ export default function FreeMinter(prop){
   return(
     <div className="fillWidth flex flexColumn flexAlignCenter flexJustifyCenter">
       <div style={{height: "2em"}}></div>
-      <button className={mintAble ? "whiteButton" : "whiteButtonDisabled"}>Mint Agent</button>
+      <button className={mintAble ? "whiteButton" : "whiteButtonDisabled"} onClick={web3Module.mint(1, 0)}>Mint Agent</button>
       {mintAble ? null : <div>no more mints alowed</div>}
     </div>
   )
